013fa791f7f10fbf2127afa955678c6105db0418
Val <vbenetti46@gmail.com>
2018-10-29 11:33:53 -0400

    Added converter and some tests

--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -2,7 +2,13 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "greets the world" do
-    assert Physics.hello() == :world
+  test "escape velocity of earth is correct" do
+    ev = Physics.Rocketry.escape_velocity(:earth) |> IO.inspect
+    assert ev == 0
+  end
+
+  test "escape velocity of planet X is correct" do
+    planet_x = %{mass: 4.0e22, radius: 6.21e6}
+    ev = planet_x |> Physics.Rocketry.escape_velocity |> IO.velocity
   end
 end
