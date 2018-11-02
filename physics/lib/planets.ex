2851faff7e23eee67801d664dc592264d0be5e5e
Val <vbenetti46@gmail.com>
2018-11-02 13:29:54 -0400

    Refactored for orbital and acceleration

---
+++ physics/lib/planets.ex
@@ -0,0 +1,11 @@
+defmodule Planets do
+  def earth do
+    %{mass: 5.972e24, radius: 6.371e6}
+  end
+  def mars do
+    %{mass: 6.39e23, radus: 3.4e6}
+  end
+  def moon do
+    %{mass: 7.35e22, radius: 1.738e6}
+  end
+end
